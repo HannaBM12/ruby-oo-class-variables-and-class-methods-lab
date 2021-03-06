6b3e018a97b193854963c8f9c6364aedda61a928
HannaBM12 <Hannabmulugeta@gmail.com>
2021-03-06 17:18:26 -0500

    commit tree 75433a8e18936e9b03c2953eb583d7a8a4adf13d

--- lib/song.rb
+++ lib/song.rb
@@ -1,44 +1,121 @@
-require 'pry'
+
 class Song
-    attr_reader :name, :artist, :genre
-
+    attr_accessor :name, :genre, :artist
     @@count = 0
-    @@artists = []
     @@genres = []
+    @@artists = []
 
-    def initialize (name, artist, genre)
-        
+    def initialize(name, artist, genre)
         @name = name
-        @artist = artist
         @genre = genre
-        @@count +=1
+        @@genres << @genre
+        @artist = artist
         @@artists << artist
-        @@genres << genre
-    
+
+        @@count +=1
+
     end
-    
+
     def self.count
         @@count
+    end
+
+    def self.genres
+        @@genres.uniq
     end
 
     def self.artists
         @@artists.uniq
     end
 
-    def self.genres
-        @@genres.uniq
-    end
-    
     def self.genre_count
-        hash = Hash.new(0)
-        @@genres.each{|k| hash[k] += 1}
-         hash
+        genre_count = {}
+        @@genres.each{|genre|
+            if genre_count[genre]
+                genre_count[genre]+=1
+            else
+                genre_count[genre] = 1
+            end
+        }
+        genre_count
     end
 
     def self.artist_count
-        hash = Hash.new(0)
-        @@artists.each{|k| hash[k] += 1}
-         hash
-    end
-
+        artist_count = {}
+        @@artists.each {|artist|
+            if artist_count[artist]
+                artist_count[artist] +=1
+            else
+                artist_count[artist] = 1
+            end
+        }
+        artist_count
 end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# require 'pry'
+# class Song
+#     attr_reader :name, :artist, :genre
+
+#     @@count = 0
+#     @@artists = []
+#     @@genres = []
+
+#     def initialize (name, artist, genre)
+        
+#         @name = name
+#         @artist = artist
+#         @genre = genre
+#         @@count +=1
+#         @@artists << artist
+#         @@genres << genre
+    
+#     end
+    
+#     def self.count
+#         @@count
+#     end
+
+#     def self.artists
+#         @@artists.uniq
+#     end
+
+#     def self.genres
+#         @@genres.uniq
+#     end
+    
+#     def self.genre_count
+#         hash = Hash.new(0)
+#         @@genres.each{|k| hash[k] += 1}
+#          hash
+#     end
+
+#     def self.artist_count
+#         hash = Hash.new(0)
+#         @@artists.each{|k| hash[k] += 1}
+#          hash
+#     end
+
+# end
